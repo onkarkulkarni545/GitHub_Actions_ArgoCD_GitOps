98d3a817fe5c19b1c8edc6629c91b4c27ac17bdf
ceptadmin <rsspandharpur12@gmail.com>
2025-09-29 14:02:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/hello', (req, res) => {
   res.send('Helloo, THis is for testing of CICD pipeline using Github-Actions argocd \n');
 });
 
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
